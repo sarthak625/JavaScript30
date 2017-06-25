8e9a881824f9f89b79f3cc864ed68fcaddde4f4b
Sarthak <sarthakn18@gmail.com>
2017-06-25 21:48:00 +0530

    Completed Day 2: JSClock

--- JSClock/script.js
+++ JSClock/script.js
@@ -1,3 +1,30 @@
+var secondhand = document.querySelector('.second-hand');
+var minuteHand = document.querySelector('.min-hand');
+var hourHand = document.querySelector('.hour-hand');
+
+
 function setDate(){
-	console.log('Hi');
+	
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const mins = now.getMinutes();
+	const hours = now.getHours();
+	
+	if ((seconds%60)-1 == 0 || seconds%60 == 0){
+		console.log('true');
+		secondhand.style.transition = null;
+	}
+	else{
+		secondhand.style.transition = "all 0.5s";
+		secondhand.style.transitionTimingFunction = 'cubic-bezier(0.1,2.7,0.58,1)';
+	}
+	const secDegrees = seconds*6 + 90;
+	const minDegrees = mins*6 + 90;
+	const hourDegrees = hours*30 + 90;
+
+	secondhand.style.transform = 'rotate('+secDegrees+'deg)';
+	minuteHand.style.transform = 'rotate('+minDegrees+'deg)';
+	hourHand.style.transform = 'rotate('+hourDegrees+'deg)';
 }
+
+setInterval(setDate,1000);
